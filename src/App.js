52842ac19c5ca1b6a4a2d3e11cc47e92fe34f231
Gonzalo Avedano <gonza.avedano@gmail.com>
2023-04-11 19:05:03 -0300

    agregado de Item, ItemCount, ItemList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBarBootstrap from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemCount from "./components/ItemCount/ItemCount";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -8,6 +9,11 @@ function App() {
     <div className="App">
       <NavBarBootstrap />
       <ItemListContainer greeting={"Bienvenidos a la Tienda Virtual"} />
+      <ItemCount
+        initial={1}
+        stock={10}
+        onAdd={(quantity) => console.log("Cantidad agregada", quantity)}
+      />
     </div>
   );
 }
