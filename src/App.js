f8dd1c7fbd94c6c4cecc12ac32d001bdb1288150
Gonzalo Avedano <gonza.avedano@gmail.com>
2023-04-18 20:46:46 -0300

    qwe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import NavBarBootstrap from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+//import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
